0698c439ee3ce24de67169c4f536b37ea88ed50c
Hanan Malka <hanan-israel.malka@hpe.com>
2017-02-19 17:10:45 +0200

    setting local storage mechanism

--- prism-ext/prism-popup.js
+++ prism-ext/prism-popup.js
@@ -1,65 +1,115 @@
 /**
  * Created by malkahan on 2/16/2017.
  */
-$(document).ready(function() {
-  var radios = document.getElementsByName("selectGroup");
-  var val = localStorage.getItem('selectGroup');
-  for (var i = 0; i < radios.length; i++) {
-    if (radios[i].value == val) {
-      radios[i].checked = true;
-    }
-  }
-  $('input[name="selectGroup"]').on('change', function() {
-    localStorage.setItem('selectGroup', $(this).val());
+
+(function() {
+
+  var config;
+
+  $(document).ready(function() {
+    config = loadFromLocalStorage();
+    drawAllUIElements(config);
   });
-});
 
-function onAhmRadioClick() {
-  var classList = {
-    allClasses: ahmShortNames,
-    activeClass: this.value
-  };
+  function addJobElementToUI(alias, checked, parentElement) {
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {data: classList}, function(response) {
+    var divElementsContainer = document.createElement('div');
+    divElementsContainer.id = alias + '-div';
+    parentElement[0].appendChild(divElementsContainer);
+
+
+    var checkboxElement = document.createElement('input');
+    var checkboxIdPrefix = 'checkbox-btn-';
+    var checkboxAttributes = {id: checkboxIdPrefix + alias, type: 'checkbox', value: alias, name: 'selectGroup', class: 'checkbox', checked: checked};
+    setAttributes(checkboxElement, checkboxAttributes);
+    divElementsContainer.appendChild(checkboxElement);
+
+
+    var labelElement = document.createElement('label');
+    var labelAttributes = {class: 'label-txt'};
+    labelElement.innerHTML = alias;
+    setAttributes(labelElement, labelAttributes);
+    divElementsContainer.appendChild(labelElement);
+
+    $('#prism-add-new-job-alias').val('');
+    $('#prism-add-new-job-url').val('');
+
+    // document.getElementById(jobName + '-div').addEventListener('change', onPrismCheckboxCheck);
+  }
+
+  function drawAllUIElements(config) {
+
+    var parentElement = $('#checkbox-container');
+    parentElement.empty();
+    for (var i = 0; i < config.jobs.length; i++) {
+      addJobElementToUI(config.jobs[i].alias, config.jobs[i].active, parentElement);
+    }
+    //TODO: Draw Counter & XPATH radio buttons
+  }
+
+//Return an object in order to create DOM
+  function loadFromLocalStorage() {
+    var config;
+    var configJson = localStorage.getItem("prism-settings");
+    if (configJson) {
+      config = JSON.parse(configJson);
+    }
+    else {
+      config = {
+        jobs: []
+      };
+    }
+    return config;
+  }
+
+//Adding the elements to local storage configuration
+  function saveToLocalStorage(config) {
+    localStorage.setItem("prism-settings", JSON.stringify(config));
+  }
+
+  function onPrismCheckboxCheck() {
+    var classList = {
+      allClasses: ahmShortNames,
+      activeClass: this.value
+    };
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {data: classList}, function(response) {
+      });
     });
-  });
-}
+  }
 
-function createDOMElement(jobName) {
-  var radioIdPrefix = "radio-btn-"
-  var divElement = document.createElement("div");
-  var radioElement = document.createElement("input");
-  var labelElement = document.createElement("label");
-  
-  //div elements set properties
-  divElement.id = jobName + "-div";
-  
-  //input element set properties
-  radioElement.id = radioIdPrefix + jobName;
-  radioElement.type = "radio";
-  radioElement.value = jobName;
-  //radioElement.name = "selectGroup";
-
-  //label element set properties
-  labelElement.innerHTML = jobName.substring(5, jobName.length);
-
-  document.getElementById("radio-container").appendChild(divElement);
-  document.getElementById(name + "-div").appendChild(radioElement);
-  document.getElementById(name + "-div").appendChild(labelElement);
-
-  document.getElementById(name).addEventListener('click', onAhmRadioClick);
-}
-
-setTimeout(function() {
-  for (var i = 0; i < ahmShortNames.length; i++) {
-    if(i===0) {
-      createDOMElement("None");
-    }
-    createDOMElement(ahmShortNames[i]);
-
-    if(i===ahmShortNames.length-1) {
-      createDOMElement("All");
+  function setAttributes(el, attrs) {
+    for (var key in attrs) {
+      $(el).attr(key, attrs[key]);
     }
   }
-}, 0);
+
+  function addNewJob() {
+    var alias = $('#prism-add-new-alias-job');
+    var url = $('#prism-add-new-url-job');
+
+    //TODO: Add validation check 
+
+    config.jobs.push({
+      alias: alias.val(),
+      url: url.val(),
+      active: true
+    });
+
+    saveToLocalStorage(config);
+    drawAllUIElements(config);
+    alias.val('');
+    url.val('');
+  }
+  
+  function deleteFromLocalStorage() {
+    config.jobs = [];
+    saveToLocalStorage(config);
+    drawAllUIElements(config);
+  }
+
+  window.addNewJob = addNewJob;
+  window.deleteFromLocalStorage = deleteFromLocalStorage;
+
+})();
